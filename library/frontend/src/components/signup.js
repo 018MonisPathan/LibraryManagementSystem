1178b5116e1ab322b9c26fa2d58604f98a0f1487
BhavikBharucha <bhavikbharucha2818@gmail.com>
2022-08-25 14:39:28 +0530

    Frontend with signup

---
+++ library/frontend/src/components/signup.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const SignUp=()=>{
+    return(
+        <div>
+            <div className="signup-form">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Registration Form</h3>
+                    </div>
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
